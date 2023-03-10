7e51395e15d2b895a3cba5bd61aa17ea67d58c5d
Baxtiyor Ergashev <ergashevb111@gmail.com>
2023-03-11 03:03:08 +0500

    paginator setting

--- Profyer/settings.py
+++ Profyer/settings.py
@@ -165,7 +165,7 @@ REST_FRAMEWORK = {
     'DEFAULT_CONTENT_LANGUAGE': 'uz',
 
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1,
+    # 'PAGE_SIZE': 1,
 
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
--- questions/views.py
+++ questions/views.py
@@ -1,10 +1,9 @@
-from rest_framework import generics, mixins, status
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import AllowAny
+from rest_framework import mixins, status
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-from .models import Page, Category, Question, UserAnswer
-from .serializer import ServiceSerializer, PageSerializer, CategorySerializer, QuestionSerializer, UserAnswerSerializer
+from .models import Page, Category, UserAnswer
+from .paginator import CustomPagination
+from .serializer import ServiceSerializer, PageSerializer, CategorySerializer, UserAnswerSerializer
 from users.models import Service
 from .tasks import add_ans
 
@@ -22,7 +21,7 @@ class PageAPIView(mixins.ListModelMixin, GenericViewSet):
 class QuestionAPIView(mixins.ListModelMixin, GenericViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    pagination_class = PageNumberPagination
+    pagination_class = CustomPagination
 
 
 class UserAnswerAPIView(mixins.CreateModelMixin, GenericViewSet):
